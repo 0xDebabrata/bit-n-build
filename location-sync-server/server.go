6ff711ae363348cef1ba8f7b2f7ab6bc3fbfd9ba
Debabrata Mondal <debabrata.js@protonmail.com>
2024-02-24 19:56:34 +0530

    init location sync server

---
+++ location-sync-server/server.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/rs/cors"
+)
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize: 1024,
+    WriteBufferSize: 1024,
+}
+
+//var watchparties = make(map[string] *Party)
+var watchparties = sync.Map{}
+
+func hello(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    fmt.Fprintf(w, "hello\n")
+}
+
+/*
+func handleCreateWatchparty(w http.ResponseWriter, r *http.Request) {
+    requestQuery := r.URL.Query()
+    ownerId := requestQuery.Get("ownerId")
+    partyId := requestQuery.Get("partyId")
+    src := requestQuery.Get("src")
+
+    if ownerId == "" {
+        log.Println("Owner ID not provided")
+        http.Error(w, "Owner ID not provided", http.StatusForbidden)
+        return
+    }
+    if partyId == "" || src == "" {
+        log.Println("Party ID or video source not provided")
+        http.Error(w, "Incomplete watchparty details provided\n", http.StatusNotFound)
+        return
+    }
+
+    party := createParty(partyId, ownerId, src)
+    watchparties.Store(partyId, party)
+    go party.run()
+
+    fmt.Fprint(w, "Success")
+}
+*/
+
+func userWebsocketHandler(w http.ResponseWriter, r *http.Request) {
+    requestQuery := r.URL.Query()
+    userId := requestQuery.Get("userId")
+
+    if userId == "" {
+        log.Println("User ID not provided")
+        return
+    }
+
+    upgrader.CheckOrigin = func(r *http.Request) bool {
+        return true
+    }
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    if userId != "" {
+        /*
+        party, ok := watchparties.Load(partyId)
+        if ok {
+            client := initClient(userId, conn, party.(*Party))  // Go type assertion
+            go client.readPump()
+            go client.writePump()
+        } else {
+            conn.Close()
+        }
+        */
+        conn.Close()
+    }
+}
+
+func adminWebsocketHandler(w http.ResponseWriter, r *http.Request) {
+    upgrader.CheckOrigin = func(r *http.Request) bool {
+        return true
+    }
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+    /*
+    party, ok := watchparties.Load(partyId)
+    if ok {
+        client := initClient(userId, conn, party.(*Party))  // Go type assertion
+        go client.readPump()
+        go client.writePump()
+    } else {
+        conn.Close()
+    }
+    */
+    conn.Close()
+}
+
+func main() {
+    mux := http.NewServeMux()
+
+    // mux.HandleFunc("/create", handleCreateWatchparty)
+    mux.HandleFunc("/admin", adminWebsocketHandler)
+    mux.HandleFunc("/user", userWebsocketHandler)
+    mux.HandleFunc("/", hello)
+
+    handler := cors.Default().Handler(mux)
+    http.ListenAndServe(":6969", handler)
+}
