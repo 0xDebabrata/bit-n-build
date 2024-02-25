c1fb532cc6951c3dec36238bbc7e74e74d62007c
dinosoupy <anishbasoup@gmail.com>
2024-02-25 07:33:07 +0530

    add layer

--- clients/pages/index.js
+++ clients/pages/index.js
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic'
+import TimeSelector from '../component/TimeSelector';
 
 // import OpenStreetMap from '../component/OpenStreetMap'
 const OpenStreetMap = dynamic(() => import('../component/OpenStreetMap'), {
@@ -9,12 +10,19 @@ const Slideover = dynamic(() => import('../component/Slideover'), { // Adjust th
   ssr: false,
 });
 
+const Breadcrumb = dynamic(() => import('../component/TimeSelector'), { // Adjust the path as necessary
+  ssr: false,
+});
+
 const index = () => {
   return (
-    <>
+    <div className='relative'>
       <OpenStreetMap />
-      <Slideover></Slideover>
-    </>
+      <div className='absolute inset-0 flex flex-col justify-between z-10'></div>
+      <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 z-20">
+        <TimeSelector />
+      </div>
+    </div>
   )
 }
 
