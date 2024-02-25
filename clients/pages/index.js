b65ae4c7f33b53e58344765a8edf8025de0d9c13
Debabrata Mondal <debabrata.js@protonmail.com>
2024-02-25 07:31:53 +0530

    update svg marker position

--- clients/pages/index.js
+++ clients/pages/index.js
@@ -5,15 +5,10 @@ const OpenStreetMap = dynamic(() => import('../component/OpenStreetMap'), {
   ssr: false,
 })
 
-const Slideover = dynamic(() => import('../component/Slideover'), { // Adjust the path as necessary
-  ssr: false,
-});
-
 const index = () => {
   return (
     <>
       <OpenStreetMap />
-      <Slideover></Slideover>
     </>
   )
 }
