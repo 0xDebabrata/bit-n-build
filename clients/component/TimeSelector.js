628c0fe53233384ae275a39e243ec1f49ffcbbfc
Debabrata Mondal <debabrata.js@protonmail.com>
2024-02-25 15:14:06 +0530

    modify timeline

--- clients/component/TimeSelector.js
+++ clients/component/TimeSelector.js
@@ -1,15 +1,11 @@
-import { useState } from "react"
-
 const timelines = ["Day", "Week", "Month"]
 
-export default function TimeSelector() {
-    const [selected, setSelected] = useState(0)
-
+export default function TimeSelector({ selectedPeriod, setSelectedPeriod }) {
     return (
         <footer className="bg-white rounded-t-xl p-4">
-            <div className="flex rounded-md justify-evenly p-2 space-x-2 rounded-md bg-gray-200">
+            <div className="flex rounded-md justify-evenly p-1 space-x-2 rounded-md bg-gray-200">
                 {timelines.map((t, idx) => (
-                  <div key={idx} onClick={() => setSelected(idx)} className={`rounded-md flex-1 py-1 text-center hover:bg-gray-200 ${selected === idx ? "bg-white" : ""}`}>{t}</div>
+                  <div key={idx} onClick={() => setSelectedPeriod(timelines[idx])} className={`rounded-md flex-1 py-1 text-center hover:bg-gray-200 ${selectedPeriod === timelines[idx] ? "bg-white" : ""}`}>{t}</div>
                 ))}
             </div>
         </footer>
